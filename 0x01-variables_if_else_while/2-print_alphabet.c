fb64ca5a9054594dccfabad032c8daa14469d4c8
Femi Mehalayese <megafemgroup@gmail.com>
2023-09-12 05:57:00 -0700

    lowcase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description: print alphabet
+ * in lower case
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a = 'a';
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar("\n");
+	return (0);
+}
