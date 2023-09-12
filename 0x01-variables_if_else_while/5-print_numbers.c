46883010f34243bbf74804cea74e3b53df1eb4cd
Femi Mehalayese <megafemgroup@gmail.com>
2023-09-12 14:05:29 -0700

    base10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description: print number
+ * base 10 0 - 9
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while(i <= 9)
+	{
+		printf("%d", i);
+		i++
+	}
+	printf("\n");
+	return (0);
+
+}
