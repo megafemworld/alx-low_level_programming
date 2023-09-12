b083645651f6145b4eebfc0d5a7f777fd51e9fbf
Femi Mehalayese <megafemgroup@gmail.com>
2023-09-12 08:41:15 -0700

    capsmall

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description: print alphahabet
+ * both capital & lower
+ * Return: 0 on success
+ */
+int main(void)
+{
+       int m = 97;
+       int n = 65;
+	while(m <= 122)
+	{
+		putchar(m);
+		m++;
+	}
+	while(n <= 90)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
