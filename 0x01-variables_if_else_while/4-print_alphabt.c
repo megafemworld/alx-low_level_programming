fda9c006d99cad9dd03837c06c2469ed6b60b122
Femi Mehalayese <megafemgroup@gmail.com>
2023-09-12 09:31:54 -0700

    skipalpha

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description: print lower case
+ * except e and q
+ * Return: 0 on success
+ */
+int main (void)
+{
+	char lower = 'a';
+	while(lower <= 'z')
+	{
+		putchar(lower);
+		lower++;
+		if (lower == 'e' || lower == 'q')
+		{
+			lower++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
