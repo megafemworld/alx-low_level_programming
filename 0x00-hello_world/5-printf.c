cd5962fcf61cfa137afebaf0232e9fd13474d7a6
Femi Mehalayese <megafemgroup@gmail.com>
2023-09-11 18:56:39 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main: Entry point
+ * Description: Print using printf fns
+ * Return: return (0) on success
+ */
+
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
