309540296591f79b476ecf1b7893bd92ba21b3c2
Femi Mehalayese <megafemgroup@gmail.com>
2023-09-12 14:21:00 -0700

    print num

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description: print number
+ * base10 using putchar
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i = 48;
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
