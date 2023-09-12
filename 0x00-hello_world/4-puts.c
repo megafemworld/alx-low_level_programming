74f34e9c46a3813f95a6c105ba894428406d7ada
Femi Mehalayese <megafemgroup@gmail.com>
2023-09-11 18:48:29 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *Main - entry point
+ *print with puts function
+ *Return 0 on success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
