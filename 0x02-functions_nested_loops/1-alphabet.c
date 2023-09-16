e91df76089f3e9b05163aac98e1f8cd159ff9aec
Femi mehalayese <megafemgroup@gmail.com>
2023-09-16 01:44:40 +0100

    alpahabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  * main - Entry point
+  * print_alphabet - print alphabet
+  * lowercase
+  * Return: Always 0.
+  */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+void print_alphabet(void)
+{
+	char alphabet = 'a';
+	while (alphabet <= z)
+	{
+		_putchar(alphabet);
+		alphabet++;
+	}
+	_putchar('\n');
+}
