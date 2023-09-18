59585dd0406cc18ec45993ac6be043e08c8b4793
Femi mehalayese <megafemgroup@gmail.com>
2023-09-18 02:45:45 +0100

    convert

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main.h"
+#include "holberton.h"
 
 /**
  * print_remaining_days - takes a date and prints how many days are
@@ -12,9 +12,9 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+	if ((year % 400 == 0 && year % 100 == 0) || year % 4 == 0)
 	{
-		if (month >= 2 && day >= 60)
+		if (month >= 3 && day >= 60)
 		{
 			day++;
 		}
@@ -24,12 +24,7 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if ((month == 2 && day > 60) || (month == 3 && day > 90) ||
-				(month == 4 && day > 120) || (month == 5 && day > 151) ||
-				(month == 6 && day > 181) || (month == 7 && day > 212) ||
-				(month == 8 && day > 243) || (month == 9 && day > 273) ||
-				(month == 10 && day > 304) || (month == 11 && day > 334) ||
-				(month == 12 && day > 365))
+		if (month == 2 && day == 60)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
