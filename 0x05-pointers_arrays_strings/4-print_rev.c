3f360372339d715496ad2743cd2767cbbbd39a5e
Femi mehalayese <megafemgroup@gmail.com>
2023-09-18 23:30:56 +0100

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * print_rev - print a string
+  * in reverse followed by a new line
+  * @s: string to be printed in reverse
+  * Return: Always 0
+  */
+
+void print_rev(char *s)
+{
+	int count = 0;
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		count++;
+		i++;
+	}
+	while (count >= 0)
+	{
+		_putchar(*(s + count));
+		count--;
+	}
+}
