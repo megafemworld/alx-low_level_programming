38f082942ce028c1d39aab71f4b595f92605e886
Femi mehalayese <megafemgroup@gmail.com>
2023-09-18 22:54:08 +0100

    count

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * _strlen - Count the length of a string
+  *
+  * @s: string to be counted
+  * Return: 0 Always.
+  */
+
+int _strlen(char *s)
+{
+	int count = 0;
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		count++;
+		i++;
+	}
+	return count;
+}
