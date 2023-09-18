a076a7ed0967748478d296fb4eca7b8a3fdadc69
Femi mehalayese <megafemgroup@gmail.com>
2023-09-18 23:58:07 +0100

    rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,15 +9,13 @@
 
 void print_rev(char *s)
 {
-	int count = 0;
 	int i = 0;
 
 	while (*(s + i) != '\0')
 	{
-		count++;
 		i++;
 	}
-	while (count >= 0)
+	while (i >= 0)
 	{
 		_putchar(*(s + count));
 		count--;
