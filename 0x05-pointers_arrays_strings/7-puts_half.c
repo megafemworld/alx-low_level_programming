71c3269eea7874d9b6a2ec83ca3e8c46edfd5a1b
Mehalayese Femi <megafemgroup@gmail.com>
2023-09-19 23:28:34 +0100

    halfp

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,15 +10,16 @@ void puts_half(char *str)
 	int i = 0;
 	int length_of_the_string = 0;
 	int n;
+	int j;
 
 	while (*(str + i) != '\0')
 	{
 		length_of_the_string++;
 	}
 	n = (length_of_the_string - 1) / 2;
-	for (n = n; n < length_of_the_string; n++)
+	for (j = n; n < length_of_the_string; j++)
 	{
-		_putchar(*(str + n));
+		_putchar(*(str + j));
 	}
 	_putchar('\n');
 
