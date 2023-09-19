c456e9a337abfb8550380f726f595283e9cdc28d
Mehalayese Femi <megafemgroup@gmail.com>
2023-09-19 10:04:51 +0100

    print skip 1

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,8 +11,14 @@ void puts2(char *str)
 
 	while (*(str + i) != '\0')
 	{
-		_putchar(*(str + i));
-		i = i + 2;
+		if (i > 0 && i % 2 > 0)
+		{
+			continue
+		}
+		else
+			_putchar(*(str + i));
+		i++;
+
 	}
 	_putchar('\n');
 }
