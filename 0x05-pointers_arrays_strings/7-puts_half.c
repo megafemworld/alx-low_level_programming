6ee7cafb92d110cbdde1933e6e864dd1dbb9c2e8
Mehalayese Femi <megafemgroup@gmail.com>
2023-09-19 23:11:57 +0100

    halfp

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,3 +1,25 @@
 #include "main.h"
 /**
- *
+ * puts_half - print half of the string
+ * print second half. if odd print last n
+ * @str: string to be divided into two
+ * Return: 0 Always.
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int length_of_the_string = 0;
+	int n;
+
+	while(*(str + i) != '\0')
+	{
+		length_of_the_string++;
+	}
+	n = (length_of_the_string - 1) / 2;
+	for(n; n < length_of the string; n++)
+	{
+		_putchar(*(str + n);
+	}
+	_putchar('\n');
+
+}
