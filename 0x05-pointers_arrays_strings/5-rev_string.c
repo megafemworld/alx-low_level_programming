f9d85ba8c2c58c5450be3b0b6346edc9e2f6d87f
Femi mehalayese <megafemgroup@gmail.com>
2023-09-19 01:29:22 +0100

    reversestring

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,16 +10,14 @@ void rev_string(char *s) {
     int i;
     char temp;
 
-    // Calculate the length of the string
     while (s[length] != '\0') {
         length++;
     }
-
-    // Swap characters to reverse the string
+    
     for (i = 0; i < length / 2; i++) {
         temp = s[i];
         s[i] = s[length - 1 - i];
         s[length - 1 - i] = temp;
     }
 }
-
+i
