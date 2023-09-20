a08998309735dec75f9a890385a8d9b831cff8e5
Femi mehalayese <megafemgroup@gmail.com>
2023-09-20 03:48:55 +0100

    array printer

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * print_array - print the nth of an array
+  * @a: the main array
+  * @n: nth elements to print
+  * Return: 0 Always.
+  */
+void print_array(int *a, int n)
+{
+	int i = 0;
+	j = n - 1;
+
+	while (i <= j)
+	{
+		printf("%d, ", a[i]);
+		i++;
+	}
+	printf("\n");
+}
