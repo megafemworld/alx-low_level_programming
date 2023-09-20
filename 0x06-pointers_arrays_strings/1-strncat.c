f0b4a90f609a64235c18d948435dc66825ac26f9
Femi mehalayese <megafemgroup@gmail.com>
2023-09-21 00:58:43 +0100

    srcncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,11 +15,10 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		i++;
 	}
-	while (src[j] < n)
+	while (src[j] != '\0' && j < n)
 	{
 		dest[i] = src[j];
-		i++;
-		j++;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
