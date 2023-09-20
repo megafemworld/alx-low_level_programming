97855c6bd16a655024d0a9094ed162bd60fc76dc
Mehalayese Femi <megafemgroup@gmail.com>
2023-09-20 19:09:05 +0100

    gg

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _strncat - concatenate two string together
+ * @dest: the string to concatenate to
+ * @src: tge string to concatenate to the end.
+ * Return: 0 Always.
+ */
+char *_strcat(char *dest, char *src)
+{
+}
