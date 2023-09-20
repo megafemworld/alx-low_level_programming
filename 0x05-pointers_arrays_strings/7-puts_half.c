426a0f30144c6073c669a5ebed3183c18bcb0921
Femi mehalayese <megafemgroup@gmail.com>
2023-09-20 02:45:07 +0100

    halfpp

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,11 +16,23 @@ void puts_half(char *str)
 		length_of_the_string++;
 		i++;
 	}
-	n = length_of_the_string / 2;
-	while (str[n] != '\0')
+	if(length_of_the_string % 2 == 0)
 	{
+		n = length_of_the_string / 2;
+		while (str[n] != '\0')
+		{
 		_putchar(str[n]);
 		n++;
+		}
+	}
+	else
+	{
+		n = (length_of_the_string) - 1 / 2;
+		while (str[n] != '\0')
+		{
+			_putchar(str[n]);
+			n++;
+		}
 	}
 	_putchar('\n');
 }
