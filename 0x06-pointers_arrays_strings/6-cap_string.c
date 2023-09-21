639eba33ac59f43df9294cc96150b45b73be8ade
Femi mehalayese <megafemgroup@gmail.com>
2023-09-21 17:36:08 +0100

    word cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+  * cap_string - capitalize all words of a string
+  * @s: string to be capitalize
+  * Return: capitalized string
+  */
+char *cap_string(char *s)
+{
+	int i = 0;
+	int target = 0;
+
+	while (s[i] != '\0')
+	{
+		if ((i == 0) && ((s[0] < 97) || s[0] > 122))
+		{
+			s[0] = s[0];
+		}
+		if else (s[0] >= 97 && s[0] <= 122)
+		{
+			s[0] = s[0] - 32;
+		}
+		if (s[i] == 32 || s[i] = '\n')
+		{
+			s[i] = s[i + 1];
+			if (s[i] >= 97 && s[i] <= 122)
+			{
+				s[i] = s[i] - 32;
+			}
+		}
+		i++;
+	}
+}
