94b54fd9c2895bef4aea4754f4f8a09e12320ecd
Mehalayese Femi <megafemgroup@gmail.com>
2023-09-21 03:35:28 +0100

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncpy - copy string in nth time
+ * @dest: destination of the character
+ * @src: string source to be copied
+ * Return: dest 
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	int j = 0;
+
+	while (src[j] != '\0')
+	{
+		j++;
+	}
+	for (i = 0; i < n; i++)
+	{
+		if (i > j)
+		{
+			dest[i] = '\0';
+		}
+		else
+			dest[i] = src[i];
+	}
+	return (dest);
+}
