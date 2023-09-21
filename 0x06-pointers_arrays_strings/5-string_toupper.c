dfaab222674c46f62291807b9578f81214beeb5a
Femi mehalayese <megafemgroup@gmail.com>
2023-09-21 16:00:47 +0100

    revrse

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  * string_toupper - Change lower string to upper
+  * 
+  * Return: Always 0.
+  */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] = s[i] - 32;
+		}
+		else if (s[i] < 97 || s[i] > 122)
+		{
+			s[i] = s[i];
+		}
+		i++;
+	}
+	return (s);
+}
