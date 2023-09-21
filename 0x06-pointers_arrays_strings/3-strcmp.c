58f26851c4e2239827d735f0fd1a5309d5195144
Femi mehalayese <megafemgroup@gmail.com>
2023-09-21 12:15:34 +0100

    comp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
-  * _strcmp - compare two string
-  * @s1: first string
-  * @s2: second string
-  * Return: 0 on equal
+ * _strcmp - compare string values
+ * @s1: input value
+ * @s2: input value
+ *
+ * Return: s1[i] - s2[i]
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i =0;
+	int i;
 
-	while (s1[i] != '\0' || s2[i] != '\0')
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-	 if (s1 != s2)
-		{
-		 return (*s1 - *s2);
-		}
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 		i++;
 	}
 	return (0);
