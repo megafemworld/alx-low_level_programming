6f287d4bc1dd8ad53ac9b84a81827f626f7eb4f2
Femi mehalayese <megafemgroup@gmail.com>
2023-09-21 12:04:52 +0100

    comp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,26 +4,26 @@
   * @s1: first string
   * @s2: second string
   * Return: 0 on equal
-  */
+ */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i =0;
 
 	while (s1[i] != '\0' || s2[i] != '\0')
 	{
-		if (s1[i] == s2[i])
+	 if (s1 == s2)
 		{
-			continue;
-		}
-		else if (s1[i] > s2[i])
+		return (0);
+		} 
+		if (s1 > s2)
 		{
-			return (1);
+		return -(*s2 - *s1);
 		}
 		else
 		{
-			return (-1);
+		return *s1 - *s2;
 		}
-		i++
+		i++;
 	}
 	return (0);
 }
