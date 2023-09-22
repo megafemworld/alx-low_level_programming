a2e3112bd7925d00ec0e4dcdfaf11fd030710611
Femi mehalayese <megafemgroup@gmail.com>
2023-09-22 04:57:11 +0100

    magic

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * leet - encode a string into 1337.
+  * @s: string to encode
+  * Return: return string
+  */
+char *leet(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == a || s[i] == A || s[i] == e || s[i] == E || s[i] == o || s[i] == o ||
+				s[i] == t || s[i] == T || s[i] == l || s[i] == L)
+
+	}
+}
