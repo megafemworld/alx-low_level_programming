68585cc1814d7b046ace3268a491870e63b0ff98
Femi mehalayese <megafemgroup@gmail.com>
2023-09-23 15:37:56 +0100

    num printer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,7 +5,21 @@
   * Return: nothing
   */
 
-void print_number(int n)
-{
-	_putchar(n + '0');
+void print_number(int n) {
+    int div = 1;
+    if (n < 0)
+    {
+	    n = -n;
+	    _putchar('-');
+    }
+    while (n / div >= 10)
+    {
+	    div = div * 10;
+    }
+    while (div > 0)
+    {
+	    _putchar((n / div) + '0');
+	    n = n % div;
+	    div = div / 10;
+    }
 }
