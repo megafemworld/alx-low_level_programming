9ed5651534c07c6b46cbd47dd14184c883b309b9
Femi mehalayese <megafemgroup@gmail.com>
2023-09-23 01:45:50 +0100

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * rot13 - encode a string using rot13
+  * @s: string to be encoded
+  * Return: return the string s
+
+char *rot13(char *s)
+{
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == input[j])
+			{
+				s[i] = output[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
