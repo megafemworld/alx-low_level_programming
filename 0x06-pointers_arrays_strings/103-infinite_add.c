840603e472684c95b0d2a4456df6ac89ab8e7646
Femi mehalayese <megafemgroup@gmail.com>
2023-09-23 17:16:52 +0100

    hh

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * infinite_add - Add two number together
+  * @n1: first number
+  * @n2: second number
+  * @r: store result
+  * @size_r: buffer size
+  * Return: alway r otherwise 0
+  */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int num1, num2, int r;
+}
