db4d01470d02077b201e8cd9e6115b499fc58bf4
Femi mehalayese <megafemgroup@gmail.com>
2023-09-24 04:25:29 +0100

    ggg

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * prints all possible combinations of
+ * single-digit numbers separated by comma
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
