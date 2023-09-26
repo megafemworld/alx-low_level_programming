f78be9b2f325b21be873bf9ff4cf1c508ded82af
Femi mehalayese <megafemgroup@gmail.com>
2023-09-26 05:29:49 +0100

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,14 +5,14 @@
   * @s: string pointer
   * @b: constant byte
   * @n: number of bytes
+  *
   * Return : return pointer s
   */
-
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n; n++)
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
