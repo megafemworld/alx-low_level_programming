64342159ac530a12a34bd2bc250ef4ca941e5e9e
Femi mehalayese <megafemgroup@gmail.com>
2023-09-26 05:47:22 +0100

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  * _strchr - locate a character in a string
+  * @s: string to search through
+  * @c: character to search for
+  * 
+  * Return: pointer or Null
+  */
+char *_strchr(char *s, char c)
+{
+	char *p = NULL;
+	unsigned int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			p = (s + i);
+			break;
+		}
+		i++
+	}
+	if (s[i] == '\0' && c == '\0')
+	{
+		p = (s + i);
+	}
+	return (p);
+}
