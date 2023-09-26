97c4d5dc163d4e076a6a73a7bbaea32f478e2dca
Mehalayese Femi <megafemgroup@gmail.com>
2023-09-26 20:15:44 +0100

    ddd

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strspn - get the length of a prexfix substring
+ * @s: string to be scannee
+ * @accept: string of characters to match 
+ * Return: number of btye from s in accept.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int j = 0, c = 0, off = 0, g = 0;
+
+	for (j = 0; s[j]; j++)
+	{
+		for (c = 0; accept[c]; c++)
+		{
+			off = 0;
+			if (s[j] == accept[c])
+			{
+				g++;
+				off = 1;
+				break;
+			}
+		}
+		if (off == 0)
+		{
+			break;
+		}
+	}
+	return (g);
+}
