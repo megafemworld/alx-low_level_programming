09ac12ba9195cbbfd7d239dceb4ca91b7dabb417
Femi mehalayese <megafemgroup@gmail.com>
2023-11-16 20:42:03 +0100

    printer

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h";
+
+/**
+  * _puts_recursion- print a string follw by new line
+  * @s: string to be print
+  *
+  * Return: string printed
+  */
+
+void _puts_recusion(char *s)
+{
+	if (s == '\0')
+	{
+		return;
+	}
+	_putchar(s);
+	_puts_recursion(s + 1);
+}
