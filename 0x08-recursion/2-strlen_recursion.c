142a611fdd558cb21afcf7f593630b15dbb6f261
Femi mehalayese <megafemgroup@gmail.com>
2023-11-16 21:59:17 +0100

    strlen rev

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  * _strlen_recursion: calculate the length of a string
+  * @s: string to calculate the length of the string
+  *
+  * Return: length of the string
+  */
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		_strlen_recursion(s + 1);
+		i = i + 1;
+	}
