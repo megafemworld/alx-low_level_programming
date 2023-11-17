08f44ac0c1dbe763d7f947236306b8f99d23486e
Femi mehalayese <megafemgroup@gmail.com>
2023-11-17 11:13:37 +0100

    ddd

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * _sqrt_recursion - Calculate the square root of a number.
+  * @n: number to find square root
+  *
+  * Return: The square root
+  */
+
+int _sqrt_recursion(int n)
+{
+	if( n == 0 | n == 1)
+		return (n);`
+}
