c00d981789f0bb4af17e421037b1b3f2a004a4ff
Femi mehalayese <megafemgroup@gmail.com>
2023-11-17 12:51:02 +0100

    sqrt

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * is_palindrome - check if a string is palindrome.
+  * @s: string to check for palindrome
+  *
+  * Return: 1 if it's palindrome, otherwise 0.
+  */
+
+int is_palindrome(char *s)
+{
+
+}
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,14 +1,29 @@
 #include "main.h"
 
 /**
-  * _sqrt_recursion - Calculate the square root of a number.
-  * @n: number to find square root
-  *
-  * Return: The square root
-  */
-
+ * sqrt2 - Makes possible to evaluate from 1 to n
+ * @a: same number as n
+ * @b: number that iterates from 1 to n
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int sqrt2(int a, int b)
+{
+	if (b * b == a)
+		return (b);
+	else if (b * b > a)
+		return (-1);
+	return (sqrt2(a, b + 1));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of n
+ * @n: Number Integer
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _sqrt_recursion(int n)
 {
-	if( n == 0 | n == 1)
-		return (n);`
+	return (sqrt2(n, 1));
 }
