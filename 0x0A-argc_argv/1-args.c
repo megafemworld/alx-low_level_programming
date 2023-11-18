1da61b8fba291eaabb15f9cab9fc91f5e710a6bc
Femi mehalayese <megafemgroup@gmail.com>
2023-11-18 16:22:54 +0100

    print total

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * main - print number of arguments
+  * @argc : argument counter
+  * @argv : argument vector
+  * Return : number counted
+  */
+
+int main(int argc)
+{
+	int i;
+	i = argc - 1;
+	_putchar('i');
+}
