9a6d8ed0e9cf59e2ecb605eb66127d7f1bdfa358
Femi mehalayese <megafemgroup@gmail.com>
2023-11-18 22:06:05 +0100

    print all

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  * main - print all argumnent received
+  * @argc: argument counter
+  * @argv: argument vector/array
+  * Return: 0 always
+  */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int j = 0;
+
+	for (i = 1; i <= argc; i++)
+	{
+		while(argv[i][j] != '\0')
+		{
+			_putchar(argv[i][j]);
+			j++;
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
