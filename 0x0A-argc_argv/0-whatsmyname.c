39c6596ba783c70eb84a411e920a8efed40ab37f
Femi mehalayese <megafemgroup@gmail.com>
2023-11-18 15:23:47 +0100

    name print

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,1 +1,22 @@
 #include "main.h"
+/**
+  * main - print name of program
+  * @agrc: argument count
+  * @argv: argumnent vector/array
+  * Return: return the name
+  */
+
+int main (int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		while (*argv[i] != '\0')
+		{
+		_putchar(*argv[i]);
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
