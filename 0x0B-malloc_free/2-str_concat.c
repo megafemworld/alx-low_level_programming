e1cba71a08ba70dfc86d3daf5a13a342081a4739
Femi mehalayese <megafemgroup@gmail.com>
2023-11-21 21:51:03 +0100

    str concact now

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -6,36 +6,34 @@
   * str_concat - concantenates two strings
   * @s1: first string
   * @s2: second string
-  * 
+  *
   * Return: NULL on failure otherwise pointer
   */
 
  char *str_concat(char *s1, char *s2)
  {
     char *t;
-    int tlen, len1, len2, i, j, n;
+    int tlen, len1, len2, i = 0, j;
     int k = 0;
 
-    if (s1 == NULL)
-        s1 = "";
-    else if (s2 == NULL)
-        s2 = "";
     len1 = strlen(s1);
     len2 = strlen(s2);
     tlen = len1 + len2 + 1;
     t = malloc(tlen * sizeof(char));
     if (t == NULL)
         return (NULL);
-    for (i = 0; i < len1; i++)
+    while (s1[i] != '\0')
     {
-        t[i] = s1[i];
-        n++;
+	    if (s1[i] == NULL)
+		    t[i] = "";
+	    else
+		    t[i] = s1[i];
+	    i++;
     }
-    n = n + 1;
-    for (j = n; j <= len2; j++)
+    for (j = i; j <= len2; j++)
     {
-        t[j] = s2[k];
-        k++;
+	    t[i] = s2[k];
+	    k++;
     }
     return (t);
  }
