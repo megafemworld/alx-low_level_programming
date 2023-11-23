cfe8df57b96fc3dd112a70d4e2709d682c15ec75
Femi mehalayese <megafemgroup@gmail.com>
2023-11-23 19:16:35 +0100

    more malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * malloc_checked - allocate memory with malloc.
+  * @b: memory space to allocate
+  *
+  * Return: on failed status value of 98 otherwise pointer
+  */
+
+void *malloc_checked(unsigned int b)
+{
+
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+More ALX task on Malloc and free
