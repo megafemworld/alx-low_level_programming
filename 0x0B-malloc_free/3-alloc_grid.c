7fed468586790e6b03bc80f70502c4361ab657e9
Mehalayese Femi <megafemgroup@gmail.com>
2023-11-23 18:49:30 +0100

    ddd

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * alloc_grid - create a pointer tp 2d array
+ * width: width of array
+ * height: height od array
+ *
+ * Return: pointer to the array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	if (width <= 0 | width == NULL | height <= 0 | height == NULL)
+		return (NULL);
+}
