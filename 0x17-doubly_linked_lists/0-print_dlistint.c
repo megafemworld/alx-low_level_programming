800e123c0585ca6c8a49220dee9398830ee02db1
Femi mehalayese <megafemgroup@gmail.com>
2024-03-22 05:37:33 +0100

    print all

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_dlistint - prints all elements of a list
+ * h : the name of the list to print through
+ *
+ * Return : the number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int i = 0;
+
+	for(struct dlistint_t *h = head; i != NULL; i = i->next)
+	{
+		print("%d\n", i->data);
+	}
+	return (i);
+}
