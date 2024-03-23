baf9ffd4535fffac0f75417ea3d7d98f459b1ae5
Femi Mehalayese <megafemgroup@gmail.com>
2024-03-23 09:12:07 +0100

    add front

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - add a new node at the beginning
+ * @head: head of the pointer
+ * @n: data to input to the node
+ * Return: Adress of new node or NULL if falied
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t newnode = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+	if(!newnode)
+	{
+		return (NULL);
+	}
+	newnode->n = n;
+	newnode->prev = NULL;
+	newnode->next =  *head);
+
+	if ((*head) != NULL)
+	{
+		(*head)->prev = node;
+	}
+	(*head) = newnode;
+	return (newnode);
+}
