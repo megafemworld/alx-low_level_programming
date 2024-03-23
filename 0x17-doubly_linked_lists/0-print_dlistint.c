cddf8fd1d3649c009e5c2f07bb4074b4e4b2a2bb
Femi Mehalayese <megafemgroup@gmail.com>
2024-03-23 07:40:55 +0100

    print and node

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,10 +9,12 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	int i = 0;
+	int count = 0;
 
 	for (struct dlistint_t *h = head; i != NULL; i = i->next)
 	{
 		printf("%d\n", i->data);
+		count++;
 	}
-	return (0);
+	return (count);
 }
