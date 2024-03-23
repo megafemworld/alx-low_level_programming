fbb5242361c2482ddd65d1c8674946265c653a71
Femi Mehalayese <megafemgroup@gmail.com>
2024-03-23 09:26:17 +0100

    add front

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,7 +9,7 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t newnode = (dlistint_t *)malloc(sizeof(dlistint_t));
+	dlistint_t* newnode = (dlistint_t *)malloc(sizeof(dlistint_t));
 
 	if(!newnode)
 	{
@@ -17,11 +17,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 	newnode->n = n;
 	newnode->prev = NULL;
-	newnode->next =  *head);
+	newnode->next =  (*head);
 
 	if ((*head) != NULL)
 	{
-		(*head)->prev = node;
+		(*head)->prev = newnode;
 	}
 	(*head) = newnode;
 	return (newnode);
