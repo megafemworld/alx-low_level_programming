4b336d12d0e7eeebe76077908a2f5d020e21a40f
Femi Mehalayese <megafemgroup@gmail.com>
2024-03-23 07:51:32 +0100

    print and node

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,7 +11,7 @@ size_t print_dlistint(const dlistint_t *h)
 	int i = 0;
 	int count = 0;
 
-	for (dlistint_t  dlistint_t *h = head; i != NULL; i = i->next)
+	for (dlistint_t *h = head; i != NULL; i = i->next)
 	{
 		printf("%d\n", i->data);
 		count++;
