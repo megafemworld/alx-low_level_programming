de6651f926ada01ee3ddc2502ed2397f04669b63
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-13 22:34:37 +0100

    check

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -20,7 +20,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	current = ht->array[index];
 	while (cuurent)
 	{
-		if (strcmp(current-key, key) == 0)
+		if (strcmp(current->key, key) == 0)
 		{
 			free(current->value);
 			current->value = strdup(value);
@@ -29,11 +29,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		current = current->next;
 	}
 	new_node = malloc(sizeof(hash_node_t));
-	if(!new_node)
+	if (!new_node)
 		return (0);
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-	new_node->next = ht->array[index];
+	new_node->ext = ht->array[index];
 	ht->array[index] = new_node;
 
 	return (1);
