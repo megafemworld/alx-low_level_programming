815f20919482251ff452dc69187b68d5ec34fdb6
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-13 20:49:04 +0100

    solve for now

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * Key_index - Computes the index dor a given key.
+ * @key: The key (string) for which to compute the index.
+ * @size: The size of the hash table array.
+ *
+ * Return: The index where teh key/value pair should be stored.
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash_value;
+
+	hash_value = hash_djb2(key);
+
+	return (has_value % size);
+
+}
---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Add an element to the hash table.
+ * @ht: The hash table to add/update the key/update in.
+ * @key: The key (string) to add.
+ * @value: The value associated with the key.
+ *
+ * Return: 1 if successfully, 0 otherwise.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node, *current;
+
+	if (!ht || !key || !value)
+		return 0;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
+	while (cuurent)
+	{
+		if (strcmp(current-key, key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			return 1;
+		}
+		current = current->next;
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if(!new_node)
+		return 0;
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return 1;
+}
