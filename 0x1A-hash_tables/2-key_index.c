9ffdce9b20d8367ed29956131a83b0fd1db163ec
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-18 21:03:32 +0100

    qq

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,10 +9,10 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int hash_value;
+	unsigned long int hash_value;
 
-    if (key == NULL || size == 0)
-        return (0);
-    hash_value = hash_djb2(key);
-    return (hash_value % size);
+	if (key == NULL || size == 0)
+		return (0);
+	hash_value = hash_djb2(key);
+	return (hash_value % size);
 }
