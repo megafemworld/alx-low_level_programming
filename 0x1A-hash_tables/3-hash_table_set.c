53061bf36edc351bbb62bf4b3d13926590a3bd85
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-19 13:24:26 +0100

    hash table set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -34,7 +34,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-	new_node->next = NULL;
+	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
+
 	return (1);
 }
