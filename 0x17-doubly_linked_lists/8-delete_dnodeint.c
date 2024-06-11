897e235cea50bb6a19b066b8b73d82c584d2e01e
Femi Mehalayese <megafemgroup@gmail.com>
2024-06-11 12:30:26 +0100

    delete a node

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -12,7 +12,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int i = 0;
 	dlistint_t *temp;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
 	temp = *head;
 	if (index == 0)
