48a283948c36ecb308f92c4bff7b405a61631a7c
Femi Mehalayese <megafemgroup@gmail.com>
2024-06-11 10:40:08 +0100

    insert at index

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position.
+ * @idx:  index of the list where the new node should be added.
+ * @n: data to insert in the new node.
+ * Return: newnode otherwise NULL.
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int tracker = 0;
+	dlistint_t *temp, *insertnode;
+
+	if (h == NULL)
+		return (NULL);
+	insertnode = (dlistint_t *)malloc(sizeof(dlistint_t));
+	if (insertnode == NULL)
+		return (NULL);
+	insertnode->n = n;
+	insertnode->next = NULL;
+	insertnode->prev = NULL;
+	if (idx == 0)
+	{
+		insertnode->next = *h;
+		if (*h != NULL)
+			(*h)->prev = insertnode;
+		*h = insertnode;
+		return (insertnode);
+	}
+	temp = *h;
+	while (temp != NULL && tracker < idx)
+	{
+		if (tracker == idx - 1)
+		{
+			insertnode->next = temp->next;
+			insertnode->prev = temp;
+			if (temp->next != NULL)
+				temp->next->prev = insertnode;
+			temp->next = insertnode;
+			return (insertnode);
+		}
+		temp = temp->next;
+		tracker++;
+	}
+	free(insertnode);
+	return (NULL);
+}
